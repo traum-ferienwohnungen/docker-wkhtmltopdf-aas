3395db4306a71487bb255e70c9243fe6cf68d35d
Fabian Beuke <beuke@traum-ferienwohnungen.de>
2016-07-11 10:56:08 +0200

    add test

---
+++ test.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import base64
+
+url = 'http://127.0.0.1:80';
+
+data = {
+        'contents': '<html>hello world</html>'.encode('base64'),
+       }
+
+headers = {
+            'Content-Type': 'application/json',
+          }
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+print response.content
